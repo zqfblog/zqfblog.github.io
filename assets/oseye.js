5c7055ca15be7c9cdb2a4d44e91bd7778aa95647
chris.zhai <zhaiqianfeng@163.com>
2019-08-25 17:03:12 +0800

    update

---
+++ assets/oseye.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?32074f7e7fd2ebbe437132d15d5dc763";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
