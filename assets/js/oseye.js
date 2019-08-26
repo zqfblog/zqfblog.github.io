1d765b5d234c660385b232c54be304f1f6a398ad
chris.zhai <zhaiqianfeng@163.com>
2019-08-26 09:23:52 +0800

    update

--- assets/js/oseye.js
+++ assets/js/oseye.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?32074f7e7fd2ebbe437132d15d5dc763";
+  hm.src = "https://hm.baidu.com/hm.js?43b5b5493803ad228d2eb33089b899a5";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
